05aee6a99bd15f8cf861889b36f624acaeb0732c
Lautaro <lautaro.lopez98@gmail.com>
2020-05-22 11:51:42 -0300

    solved bugs that makes us embarrass in the presentation

--- resources/views/products/show.blade.php
+++ resources/views/products/show.blade.php
@@ -6,16 +6,26 @@
 
     <div class="container row">
         <div class="col-12 col-sm-6 mt-5 mb-5 image-container-product">
-            <img class="image-product" src="/storage/product_pictures/{{$product->img_url }}" alt="{{$product->name}}">
+            @if($product->stock !== 0)
+                <img class="image-product" src="/storage/product_pictures/{{$product->img_url }}" alt="{{$product->name}}">
+            @else
+                <img class="image-product no-stock" src="/storage/product_pictures/{{$product->img_url }}" alt="{{$product->name}}">
+            @endif
         </div>
         <div class="col-12 mt-5 col-sm-6 mb-5 pl-5">
             <h2 class="name-product">{{$product->name}}</h2>
             <p class="price-product">$ {{ $product->price }}</p>
-            <p><small>Stock {{ $product->stock}}</small></p>
+            @if($product->stock !== 0)
+                <p><small>Stock {{ $product->stock}}</small></p>
+            @else
+                <p style="color: red"><small>No hay stock</small></p>
+            @endif
             @if ($category)
                 <p><small>Category: {{ $category  }}</small></p>
             @endif
-            <a href=" {{ route('addToCart', $product ) }} " class="btn radius-none">Agregar al carrito</a>
+            @if($product->stock !== 0)
+                <a href=" {{ route('addToCart', $product ) }} " class="btn radius-none">Agregar al carrito</a>
+            @endif
         </div>
     </div>
     <div class="container mb-5">
@@ -28,8 +38,17 @@
             @foreach($products as $product)
                 <a href="{{$product->url}}" class="a-none-default">
                     <div class="product-relation">
-                        <img src="/storage/product_pictures/{{$product->img_url }}" alt="{{$product->name}}">
-                        <h5>$ {{$product->price}}</h5>
+                        @if($product->stock !== 0)
+                            <img src="/storage/product_pictures/{{$product->img_url }}" alt="{{$product->name}}">
+                        @else
+                            <img class="no-stock" src="/storage/product_pictures/{{$product->img_url }}" alt="{{$product->name}}">
+                        @endif
+                        @if($product->stock !== 0)
+                            <h5>$ {{$product->price}}</h5>
+                        @else
+                            <h5 style="color: red">SIN STOCK</h5>
+                        @endif
+                        
                         <p>{{$product->name}}</p>
                     </div>
                 </a>
--- storage/framework/views/1c0fe2896673781818810cc92828439e4001b6be.php
+++ storage/framework/views/1c0fe2896673781818810cc92828439e4001b6be.php
@@ -4,16 +4,26 @@
 
     <div class="container row">
         <div class="col-12 col-sm-6 mt-5 mb-5 image-container-product">
-            <img class="image-product" src="/storage/product_pictures/<?php echo e($product->img_url); ?>" alt="<?php echo e($product->name); ?>">
+            <?php if($product->stock !== 0): ?>
+                <img class="image-product" src="/storage/product_pictures/<?php echo e($product->img_url); ?>" alt="<?php echo e($product->name); ?>">
+            <?php else: ?>
+                <img class="image-product no-stock" src="/storage/product_pictures/<?php echo e($product->img_url); ?>" alt="<?php echo e($product->name); ?>">
+            <?php endif; ?>
         </div>
         <div class="col-12 mt-5 col-sm-6 mb-5 pl-5">
             <h2 class="name-product"><?php echo e($product->name); ?></h2>
             <p class="price-product">$ <?php echo e($product->price); ?></p>
-            <p><small>Stock <?php echo e($product->stock); ?></small></p>
+            <?php if($product->stock !== 0): ?>
+                <p><small>Stock <?php echo e($product->stock); ?></small></p>
+            <?php else: ?>
+                <p style="color: red"><small>No hay stock</small></p>
+            <?php endif; ?>
             <?php if($category): ?>
                 <p><small>Category: <?php echo e($category); ?></small></p>
             <?php endif; ?>
-            <a href=" <?php echo e(route('addToCart', $product )); ?> " class="btn radius-none">Agregar al carrito</a>
+            <?php if($product->stock !== 0): ?>
+                <a href=" <?php echo e(route('addToCart', $product )); ?> " class="btn radius-none">Agregar al carrito</a>
+            <?php endif; ?>
         </div>
     </div>
     <div class="container mb-5">
@@ -26,8 +36,17 @@
             <?php $__currentLoopData = $products; $__env->addLoop($__currentLoopData); foreach($__currentLoopData as $product): $__env->incrementLoopIndices(); $loop = $__env->getLastLoop(); ?>
                 <a href="<?php echo e($product->url); ?>" class="a-none-default">
                     <div class="product-relation">
-                        <img src="/storage/product_pictures/<?php echo e($product->img_url); ?>" alt="<?php echo e($product->name); ?>">
-                        <h5>$ <?php echo e($product->price); ?></h5>
+                        <?php if($product->stock !== 0): ?>
+                            <img src="/storage/product_pictures/<?php echo e($product->img_url); ?>" alt="<?php echo e($product->name); ?>">
+                        <?php else: ?>
+                            <img class="no-stock" src="/storage/product_pictures/<?php echo e($product->img_url); ?>" alt="<?php echo e($product->name); ?>">
+                        <?php endif; ?>
+                        <?php if($product->stock !== 0): ?>
+                            <h5>$ <?php echo e($product->price); ?></h5>
+                        <?php else: ?>
+                            <h5 style="color: red">SIN STOCK</h5>
+                        <?php endif; ?>
+                        
                         <p><?php echo e($product->name); ?></p>
                     </div>
                 </a>
